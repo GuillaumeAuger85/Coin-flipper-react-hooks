9aee46245150e189b022f6c85706f32d1148bd3b
GuillaumeAuger85 <auger.guillaume@gmail.com>
2023-02-28 22:57:21 -0500

    Modify app.js, Coin.js, Deck.js Add styles, font and images

--- src/Coin.js
+++ src/Coin.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Coin.css'
 
 function Coin(props) {
+    console.log(props.faceA)
     return (
-        <div className='Coin'>
-            <img src={props.face} />
+        <div className={`Coin  ${props.isRolling ? 'Coin-animation' : ''}`}>
+            <img className='Coin-faceA' src={props.faceA} alt={props.faceA === '/static/media/head.b9c214ceb0bfbd8f893c.jpg' ? 'head':'tail'}/>
+            <img className='Coin-faceB' src={props.faceB} alt={props.faceB ===  '/static/media/head.b9c214ceb0bfbd8f893c.jpg' ? 'head':'tail'}/>
         </div>
     )
 }
--- src/Deck.js
+++ src/Deck.js
@@ -1,29 +1,36 @@
 import { useState } from 'react';
 import useIncrement from './hooks/useIncrement';
+import './Deck.css';
+import head from './images/Head.png';
+import tail from './images/Tail.png'
 import Coin from './Coin';
 
-const coin = [
-    'https://media.geeksforgeeks.org/wp-content/uploads/20200916123059/SHalfDollarObverse2016head-200x200.jpg',
-    'https://media.geeksforgeeks.org/wp-content/uploads/20200916123125/tails-200x200.jpg'
-]
 
-function Deck({ defaultFace = coin[0] }) {
-    const [face, setFace] = useState(defaultFace);
+const coin = [head, tail]
+
+function Deck() {
+    const [faceA, setFaceA] = useState(coin[0]);
     const [nHead, incrementnHead] = useIncrement(0);
-    const [nTail, incrementnTail] = useIncrement(0);
     const [flips, incrementFlips] = useIncrement(0);
-    const getFace = () => {
+    const [isRolling, setIsRolling] = useState(false);
+    const throwCoin = () => {
+        setIsRolling(true)
         const rand = Math.floor(Math.random() * 2);
-        setFace(coin[rand]);
-        incrementFlips();
-        rand < 1 ? incrementnHead() : incrementnTail();
+        setFaceA(coin[rand]);
+        setTimeout(() => {
+            setIsRolling(false);
+            incrementFlips();
+            rand < 1 && incrementnHead();
+        }, 2000)
     }
     return (
         <div className='Deck'>
-            <h1>Let's flip a coin!</h1>
-            <Coin face={face} />
-            <button onClick={() => getFace()}>flip meeeee</button>
-            <p>Out of {flips} flips, there have beeen {nHead} heads and {nTail} tails</p>
+            <h1>Toss a coin!</h1>
+            <div className={`Deck-container ${isRolling? "Deck-container-animation": ""}`}>
+                <Coin faceA={faceA} faceB={coin[1 - coin.indexOf(faceA)]} isRolling={isRolling} />
+                <p>Out of {flips} flips, there have beeen {nHead} heads and {flips - nHead} tails</p>
+            </div>
+            <button disabled={isRolling} onClick={() => throwCoin()}>flip meeeee</button>
         </div>
     )
 }
